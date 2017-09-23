7a6a10bd891f7c53a09ad52fc2a38da5abd2ee0f
Arun Mathew Kurian <amkurian@gmail.com>
2017-09-23 19:29:36 +0530

    Pins CRUD implemented

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,44 @@
+class PinsController < ApplicationController
+	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	def index
+		@pins = Pin.all.order("created_at DESC")
+	end
+
+	def new
+		@pin = Pin.new
+	end
+
+	def edit
+	end
+
+	def update
+	 if @pin.update(pin_params)
+			redirect_to @pin, notice: "Updated Saved"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pin.destroy
+		redirect_to root_path
+	end
+
+	def create
+		@pin = Pin.new(pin_params)
+		if @pin.save
+			redirect_to @pin, notice: "Successfully Saved"
+		else
+			render 'new'
+		end
+	end
+
+	private
+  def pin_params
+  	params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+  	@pin = Pin.find(params[:id])
+  end
+end
